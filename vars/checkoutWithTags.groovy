de54293ac6bfb1b3db047a4060a1013e4c5316aa
Lars Bilke <lars.bilke@ufz.de>
2017-11-01 12:07:42 +0100

    Removed obsolete checkoutWithTags.

--- vars/checkoutWithTags.groovy
+++
@@ -1,8 +0,0 @@
-def call(String url = 'https://github.com/ufz/ogs.git') {
-    def cmd = "git fetch ${url} --tags"
-	checkout scm
-    if(isUnix())
-        sh "${cmd}"
-    else
-        bat "${cmd}"
-}
