c3ae39c4645a4e26fe714c6e5fa3e8540f537bf6
Lars Bilke <lars.bilke@ufz.de>
2018-06-21 15:49:04 +0200

    test

--- vars/conanBuild.groovy
+++ vars/conanBuild.groovy
@@ -24,28 +24,20 @@ def call(String reference) {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_CLANG_VERSIONS = "3.9"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
-              script {
-                withEnv(['CONAN_LOGIN_USERNAME=$JFROG_USR', 'CONAN_PASSWORD=$JFROG_PSW']) {
-                  sh 'printenv'
-                  sh 'sudo pip install bincrafters-package-tools'
-                  sh "conan remove -f ${reference}"
-                  sh "conan remote add upload_repo $CONAN_UPLOAD"
-                  sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
-                  sh 'python build.py'
-                  sh 'rm -r $CONAN_USER_HOME'
-                }
-              }
+              sh "rm -rf $WORKSPACE/conan"
+              sh 'sudo pip install --upgrade conan'
+              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
+              sh 'conan user'
+              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
+              sh "rm -rf $WORKSPACE/conan"
             }
-            post { always { cleanWs() } }
           }
           // *************************** gcc49 ************************************
           stage('gcc49') {
@@ -53,7 +45,7 @@ def call(String reference) {
               docker {
                 image 'ogs6/conan_gcc49'
                 label 'docker'
-                args '-v /home/jenkins/.ccache:/usr/src/.ccache'
+                // args '-v /home/jenkins/.ccache:/usr/src/.ccache'
                 alwaysPull true
               }
             }
@@ -61,27 +53,20 @@ def call(String reference) {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_GCC_VERSIONS = "4.9"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
-              script {
-                withEnv(['CONAN_LOGIN_USERNAME=$JFROG_USR', 'CONAN_PASSWORD=$JFROG_PSW']) {
-                  sh 'sudo pip install bincrafters-package-tools'
-                  sh "conan remove -f ${reference}"
-                  sh "conan remote add upload_repo $CONAN_UPLOAD"
-                  sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
-                  sh 'python build.py'
-                  sh 'rm -r $CONAN_USER_HOME'
-                }
-              }
+              sh "rm -rf $WORKSPACE/conan"
+              sh 'sudo pip install --upgrade conan'
+              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
+              sh 'conan user'
+              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
+              sh "rm -rf $WORKSPACE/conan"
             }
-            post { always { cleanWs() } }
           }
           // *************************** gcc5 ************************************
           stage('gcc5') {
@@ -97,27 +82,20 @@ def call(String reference) {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_GCC_VERSIONS = "5"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
-              script {
-                withEnv(['CONAN_LOGIN_USERNAME=$JFROG_USR', 'CONAN_PASSWORD=$JFROG_PSW']) {
-                  sh 'sudo pip install bincrafters-package-tools'
-                  sh "conan remove -f ${reference}"
-                  sh "conan remote add upload_repo $CONAN_UPLOAD"
-                  sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
-                  sh 'python build.py'
-                  sh 'rm -r $CONAN_USER_HOME'
-                }
-              }
+              sh "rm -rf $WORKSPACE/conan"
+              sh 'sudo pip install --upgrade conan'
+              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
+              sh 'conan user'
+              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
+              sh "rm -rf $WORKSPACE/conan"
             }
-            post { always { cleanWs() } }
           }
           // *************************** gcc6 ************************************
           stage('gcc6') {
@@ -133,27 +111,20 @@ def call(String reference) {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_GCC_VERSIONS = "6"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
-              script {
-                withEnv(['CONAN_LOGIN_USERNAME=$JFROG_USR', 'CONAN_PASSWORD=$JFROG_PSW']) {
-                  sh 'sudo pip install bincrafters-package-tools'
-                  sh "conan remove -f ${reference}"
-                  sh "conan remote add upload_repo $CONAN_UPLOAD"
-                  sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
-                  sh 'python build.py'
-                  sh 'rm -r $CONAN_USER_HOME'
-                }
-              }
+              sh "rm -rf $WORKSPACE/conan"
+              sh 'sudo pip install --upgrade conan'
+              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
+              sh 'conan user'
+              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
+              sh "rm -rf $WORKSPACE/conan"
             }
-            post { always { cleanWs() } }
           }
           // *************************** gcc7 ************************************
           stage('gcc7') {
@@ -169,27 +140,20 @@ def call(String reference) {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_GCC_VERSIONS = "7"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
-              script {
-                withEnv(['CONAN_LOGIN_USERNAME=$JFROG_USR', 'CONAN_PASSWORD=$JFROG_PSW']) {
-                  sh 'sudo pip install bincrafters-package-tools'
-                  sh "conan remove -f ${reference}"
-                  sh "conan remote add upload_repo $CONAN_UPLOAD"
-                  sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
-                  sh 'python build.py'
-                  sh 'rm -r $CONAN_USER_HOME'
-                }
-              }
+              sh "rm -rf $WORKSPACE/conan"
+              sh 'sudo pip install --upgrade conan'
+              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
+              sh 'conan user'
+              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
+              sh "rm -rf $WORKSPACE/conan"
             }
-            post { always { cleanWs() } }
           }
           // ************************** vs2017 ***********************************
           stage('vs2017') {
@@ -198,17 +162,16 @@ def call(String reference) {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_VISUAL_VERSIONS = "15"
               CONAN_USER_HOME = "$WORKSPACE\\conan"
               CONAN_ARCHS = "x86_64"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
               script {
                 withEnv(['CONAN_LOGIN_USERNAME=%JFROG_USR%', 'CONAN_PASSWORD=%JFROG_PSW%']) {
+                  bat 'rd /S /Q %CONAN_USER_HOME%'
                   sh "conan remove -f ${reference}"
                   sh "conan remote add upload_repo $CONAN_UPLOAD"
                   sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
@@ -217,34 +180,25 @@ def call(String reference) {
                 }
               }
             }
-            post { always { cleanWs() } }
           }
           // ************************** macos ***********************************
           stage('macos') {
-            agent {label 'mac && conan' }
+            agent { label 'mac && conan' }
             environment {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
               CONAN_USERNAME = "bilke"
-              CONAN_CHANNEL = "testing"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_APPLE_CLANG_VERSIONS = "9.1"
-              CONAN_SKIP_CHECK_CREDENTIALS = "1"
             }
             steps {
-              script {
-                withEnv(['CONAN_LOGIN_USERNAME=$JFROG_USR', 'CONAN_PASSWORD=$JFROG_PSW']) {
-                  sh "conan remove -f ${reference}"
-                  sh "conan remote add upload_repo $CONAN_UPLOAD"
-                  sh "conan user $JFROG_USR -p $JFROG_PSW -r upload_repo"
-                  sh 'python3 build.py'
-                  sh 'rm -rf %CONAN_USER_HOME%'
-                }
-              }
+              sh "rm -rf $WORKSPACE/conan"
+              sh 'conan user'
+              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python3 build.py"
+              sh "rm -rf $WORKSPACE/conan"
             }
-            post { always { cleanWs() } }
           }
         } // end parallel
       }
