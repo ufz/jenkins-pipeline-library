fd6543ae8fa4bc51800bd610b7747bd30f54b52d
Lars Bilke <lars.bilke@ufz.de>
2019-06-12 09:30:50 +0200

    Updated conanBuild.

--- vars/conanBuild.groovy
+++ vars/conanBuild.groovy
@@ -2,25 +2,30 @@
 
 def call(String reference,
   Boolean clang = true, Boolean gcc = true, Boolean win = true, Boolean mac = true) {
-
   pipeline {
     agent none
     options {
       ansiColor('xterm')
       timestamps()
     }
+    parameters {
+      booleanParam(name: 'clang', defaultValue: clang)
+      booleanParam(name: 'gcc', defaultValue: gcc)
+      booleanParam(name: 'win', defaultValue: win)
+      booleanParam(name: 'mac', defaultValue: mac)
+    }
     stages {
       stage('Build') {
         parallel {
-          // ************************** glang39 ********************************
-          stage('clang39') {
+          // ************************** glang7 *********************************
+          stage('clang7') {
             when {
               beforeAgent true
-              expression { return clang }
+              expression { return params.clang }
             }
             agent {
               docker {
-                image 'ogs6/conan_clang39'
+                image 'conanio/clang7'
                 label 'docker'
                 args '--security-opt seccomp:unconfined'
                 alwaysPull true
@@ -32,7 +37,7 @@ def call(String reference,
               CONAN_USERNAME = "bilke"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
-              CONAN_CLANG_VERSIONS = "3.9"
+              CONAN_CLANG_VERSIONS = "7"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
               CONAN_UPLOAD_RETRY = "5"
@@ -40,24 +45,20 @@ def call(String reference,
             }
             steps {
               sh "rm -rf $WORKSPACE/conan"
-              sh 'sudo pip install --upgrade conan'
-              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
-              sh 'conan user'
               sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
               sh "rm -rf $WORKSPACE/conan"
             }
           }
-          // *************************** gcc49 ************************************
-          stage('gcc49') {
+          // *************************** gcc7 ************************************
+          stage('gcc7') {
             when {
               beforeAgent true
-              expression { return gcc }
+              expression { return params.gcc }
             }
             agent {
               docker {
-                image 'ogs6/conan_gcc49'
+                image 'conanio/gcc7'
                 label 'docker'
-                // args '-v /home/jenkins/.ccache:/usr/src/.ccache'
                 alwaysPull true
               }
             }
@@ -67,7 +68,7 @@ def call(String reference,
               CONAN_USERNAME = "bilke"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
-              CONAN_GCC_VERSIONS = "4.9"
+              CONAN_GCC_VERSIONS = "7"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
               CONAN_UPLOAD_RETRY = "5"
@@ -75,24 +76,20 @@ def call(String reference,
             }
             steps {
               sh "rm -rf $WORKSPACE/conan"
-              sh 'sudo pip install --upgrade conan'
-              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
-              sh 'conan user'
               sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
               sh "rm -rf $WORKSPACE/conan"
             }
           }
-          // *************************** gcc5 ************************************
-          stage('gcc5') {
+          // *************************** gcc8 ************************************
+          stage('gcc8') {
             when {
               beforeAgent true
-              expression { return gcc }
+              expression { return params.gcc }
             }
             agent {
               docker {
-                image 'ogs6/conan_gcc5'
+                image 'conanio/gcc8'
                 label 'docker'
-                args '-v /home/jenkins/.ccache:/usr/src/.ccache'
                 alwaysPull true
               }
             }
@@ -102,7 +99,7 @@ def call(String reference,
               CONAN_USERNAME = "bilke"
               CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
               CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
-              CONAN_GCC_VERSIONS = "5"
+              CONAN_GCC_VERSIONS = "8"
               CONAN_USER_HOME = "$WORKSPACE/conan"
               CONAN_ARCHS = "x86_64"
               CONAN_UPLOAD_RETRY = "5"
@@ -110,22 +107,19 @@ def call(String reference,
             }
             steps {
               sh "rm -rf $WORKSPACE/conan"
-              sh 'sudo pip install --upgrade conan'
-              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
-              sh 'conan user'
               sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
               sh "rm -rf $WORKSPACE/conan"
             }
           }
-          // *************************** gcc6 ************************************
-          stage('gcc6') {
+          // *************************** gcc9 ************************************
+          stage('gcc9') {
             when {
               beforeAgent true
-              expression { return gcc }
+              expression { return params.gcc }
             }
             agent {
               docker {
-                image 'ogs6/conan_gcc6'
+                image 'conanio/gcc9'
                 label 'docker'
                 args '-v /home/jenkins/.ccache:/usr/src/.ccache --security-opt seccomp:unconfined'
                 alwaysPull true
@@ -145,44 +139,6 @@ def call(String reference,
             }
             steps {
               sh "rm -rf $WORKSPACE/conan"
-              sh 'sudo pip install --upgrade conan'
-              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
-              sh 'conan user'
-              sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
-              sh "rm -rf $WORKSPACE/conan"
-            }
-          }
-          // *************************** gcc7 ************************************
-          stage('gcc7') {
-            when {
-              beforeAgent true
-              expression { return gcc }
-            }
-            agent {
-              docker {
-                image 'ogs6/conan_gcc7'
-                label 'docker'
-                args '-v /home/jenkins/.ccache:/usr/src/.ccache --security-opt seccomp:unconfined'
-                alwaysPull true
-              }
-            }
-            environment {
-              CONAN_REFERENCE = "${reference}"
-              JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
-              CONAN_USERNAME = "bilke"
-              CONAN_UPLOAD = "https://ogs.jfrog.io/ogs/api/conan/conan"
-              CONAN_STABLE_BRANCH_PATTERN = "release/*|stable/*"
-              CONAN_GCC_VERSIONS = "7"
-              CONAN_USER_HOME = "$WORKSPACE/conan"
-              CONAN_ARCHS = "x86_64"
-              CONAN_UPLOAD_RETRY = "5"
-              CONAN_BUILD_POLICY = "outdated"
-            }
-            steps {
-              sh "rm -rf $WORKSPACE/conan"
-              sh 'sudo pip install --upgrade conan'
-              sh 'sudo pip install conan_package_tools bincrafters-package-tools'
-              sh 'conan user'
               sh "CONAN_LOGIN_USERNAME=$JFROG_USR CONAN_PASSWORD=$JFROG_PSW python build.py"
               sh "rm -rf $WORKSPACE/conan"
             }
@@ -191,7 +147,7 @@ def call(String reference,
           stage('vs2017') {
             when {
               beforeAgent true
-              expression { return win }
+              expression { return params.win }
             }
             agent {label 'win && conan' }
             environment {
@@ -220,9 +176,9 @@ def call(String reference,
           stage('macos') {
             when {
               beforeAgent true
-              expression { return mac }
+              expression { return params.mac }
             }
-            agent { label 'mac && conan' }
+            agent { label 'mac' }
             environment {
               CONAN_REFERENCE = "${reference}"
               JFROG = credentials('3a3e2a63-4509-43c9-a2e9-ea0c50fdcd4c')
